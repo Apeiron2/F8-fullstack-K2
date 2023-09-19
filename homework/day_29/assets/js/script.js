606a8e9b08b973b2873e70b5ed8c816e96a63243
Apeiron2 <apeironisme@gmail.com>
2023-09-19 18:51:49 +0700

    git

--- homework/day_29/assets/js/script.js
+++ homework/day_29/assets/js/script.js
@@ -16,45 +16,13 @@ function updateTable() {
     item.dataset.yBottom = item.offsetTop + item.clientHeight / 4;
   });
 }
+
 function addEventDrag(element) {
   var innerX;
   var innerY;
-  var isFinal = false;
   element.addEventListener("drag", function (e) {
     this.style.opacity = 0.3;
-    var currentY = e.y - innerY;
-    var currentIndex;
-    //Hành động kéo xuống
-    if (e.offsetY > 0) {
-      currentIndex = Array.from(itemList).findLast(function (item, index) {
-        var limitTop = itemList[index - 1]
-          ? itemList[index - 1].dataset.yTop
-          : 0;
-        if (currentY >= limitTop) {
-          return item;
-        }
-      });
-    }
-    //Hành động kéo lên
-    if (e.offsetY < 0) {
-      currentIndex = Array.from(itemList).find(function (item, index) {
-        var limitBottom = item.dataset.yBottom;
-        if (currentY <= limitBottom) {
-          return item;
-        }
-      });
-    }
-    // console.log(currentIndex);
-    console.log(isFinal);
-    if (isFinal) {
-      element.parentElement.insertBefore(
-        itemList[itemList.length - 1],
-        element.nextSibling
-      );
-    } else {
-      container.insertBefore(element, currentIndex);
-    }
-    updateTable();
+    actionDrag(element, e, innerY);
   });
   element.addEventListener("dragstart", function (e) {
     innerX = e.offsetX;
@@ -62,38 +30,33 @@ function addEventDrag(element) {
   });
   element.addEventListener("dragend", function (e) {
     this.style.opacity = 1;
-    var currentY = e.y - innerY;
-    var currentIndex;
-    //Hành động kéo xuống
-    if (e.offsetY > 0) {
-      currentIndex = Array.from(itemList).findLast(function (item, index) {
-        var limitTop = itemList[index - 1]
-          ? itemList[index - 1].dataset.yTop
-          : 0;
-        if (currentY >= limitTop) {
-          return item;
-        }
-      });
-    }
-    //Hành động kéo lên
-    if (e.offsetY < 0) {
-      currentIndex = Array.from(itemList).find(function (item, index) {
-        var limitBottom = item.dataset.yBottom;
-        if (currentY <= limitBottom) {
-          return item;
-        }
-      });
-    }
-    // console.log(currentIndex);
-    console.log(isFinal);
-    if (isFinal) {
-      element.parentElement.insertBefore(
-        itemList[itemList.length - 1],
-        element.nextSibling
-      );
-    } else {
-      container.insertBefore(element, currentIndex);
-    }
-    updateTable();
+    actionDrag(element, e, innerY);
   });
+}
+
+function actionDrag(element, e, innerY) {
+  var currentY = e.y - innerY;
+  var currentIndex;
+  //Hành động kéo xuống
+  if (e.offsetY > 0) {
+    currentIndex = Array.from(itemList).findLast(function (item, index) {
+      var limitTop = itemList[index - 1] ? itemList[index - 1].dataset.yTop : 0;
+      if (currentY >= limitTop) {
+        return item;
+      }
+    });
+  }
+  //Hành động kéo lên
+  if (e.offsetY < 0) {
+    currentIndex = Array.from(itemList).find(function (item, index) {
+      var limitBottom = item.dataset.yBottom;
+      if (currentY <= limitBottom) {
+        return item;
+      }
+    });
+  }
+
+  container.insertBefore(element, currentIndex);
+
+  updateTable();
 }
